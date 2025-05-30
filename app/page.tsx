79a3ae090a90b47fc27c67c376609f270acef36f
CodeForgeNet <codeforgenet@icloud.com>
2025-05-31 01:02:30 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Footer from "./_components/Footer";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      {/* HEADER */}
+      <Header />
+
+      {/* Hero */}
+      <Hero />
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
